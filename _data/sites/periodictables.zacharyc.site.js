7d19921e41b1ed4d1b1bcb5adecef3dd1851940b
Zachary Crespin <Zachary.lights@gmail.com>
2022-08-10 13:48:49 -0700

    update frequency

--- _data/sites/periodictables.zacharyc.site.js
+++ _data/sites/periodictables.zacharyc.site.js
@@ -3,7 +3,7 @@ module.exports = {
 	description: "periodictables.zacharyc.site",
 	skip: false,
 	options: {
-		frequency: 60,
+		frequency: 1440,
 		// Use "run" if the sites don’t share assets on the same origin
 		//           and we can reset chrome with each run instead of
 		//           each site in every run (it’s faster)
